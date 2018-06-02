a5ba4a4281a4a06e5e69f80dae769f46d7baeeec
Robert Marchese <bobm123@gmail.com>
2018-06-02 10:30:23 -0400

    Basic working example of my app

--- audio_viewer/audio_viewer.py
+++ audio_viewer/audio_viewer.py
@@ -1,3 +1,91 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+import os
+import toga
+from toga.style.pack import *
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import numpy as np
+import wave
+
+import pyaudio
+
+
+def GenerateSpectrum(filename):
+    file = wave.open(filename, "rb")
+    data = file.readframes(40000)
+    data = np.fromstring(data, 'Int16')
+    data = data.astype(float)
+    NFFT = 128 
+    Fs = 8000
+    dt = 1.0/Fs
+    t = np.arange(0.0, len(data)/Fs, dt)
+
+    ax1 = plt.subplot(211)
+    plt.plot(t, data)
+    ax2 = plt.subplot(212, sharex=ax1)
+    Pxx, freqs, bins, im = plt.specgram(data, NFFT=NFFT, Fs=Fs, noverlap=64)
+    plt.savefig('Figure_temp.png')
+
+
+class SpectrumApp(toga.App):
+
+    filename = "resources/laurel8k.wav"
+    file = wave.open(filename, "rb")
+    paud = pyaudio.PyAudio()
+    format = paud.get_format_from_width(file.getsampwidth())
+    #open stream  
+    stream = paud.open(format = format,  
+                    channels = file.getnchannels(),  
+                    rate = file.getframerate(),  
+                    output = True)
+
+    def playback(self, widget):
+        chunk = 1024
+
+        print("play")
+        self.file.rewind()
+        data = self.file.readframes(chunk)  
+        while data:
+            self.stream.write(data)  
+            data = self.file.readframes(chunk)
+
+    def startup(self):
+        self.main_window = toga.MainWindow(title=self.name, size=(660, 520))
+        self.ScrollContainer = toga.ScrollContainer()
+
+        main_box = toga.Box()
+        plot_box = toga.Box()
+
+        main_box.style.padding = 10
+        main_box.style.update(alignment=CENTER)
+        main_box.style.update(direction=COLUMN)
+        
+        button = toga.Button('Play', on_press=self.playback, style=Pack(width=100))
+
+        GenerateSpectrum(self.filename) 
+        image_from_path = toga.Image('Figure_temp.png')
+        imageview_from_path = toga.ImageView(image_from_path)
+        imageview_from_path.style.update(height=480)
+        imageview_from_path.style.update(width=640)
+        plot_box.add(imageview_from_path)
+        
+        main_box.add(button)
+        main_box.add(plot_box)
+
+        self.main_window.content = main_box
+
+
+        self.main_window.show()
+
+def main():
+    return SpectrumApp('Spectrum', 'org.spectrum')
+
+
+if __name__ == '__main__':
+    app = main()
+    app.main_loop()
