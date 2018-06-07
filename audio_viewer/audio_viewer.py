c5a2142418619f767c4c42d61945fb10eef0b1ae
Robert Marchese <bobm123@gmail.com>
2018-06-07 08:34:21 -0400

    Fix some issues Flake8 found

--- audio_viewer/audio_viewer.py
+++ audio_viewer/audio_viewer.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
-import os
 import toga
-from toga.style.pack import *
+from toga.style.pack import Pack, CENTER, COLUMN
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 import numpy as np
-import wave
-
 import pyaudio
+import wave
 
 
 def GenerateSpectrum(filename):
@@ -20,7 +18,7 @@ def GenerateSpectrum(filename):
     data = file.readframes(40000)
     data = np.fromstring(data, 'Int16')
     data = data.astype(float)
-    NFFT = 128 
+    NFFT = 128
     Fs = 8000
     dt = 1.0/Fs
     t = np.arange(0.0, len(data)/Fs, dt)
@@ -32,7 +30,7 @@ def GenerateSpectrum(filename):
     plt.savefig('Figure_temp.png')
 
 
-#TODO: Add a file browser widget for loading the audio content
+# TODO: Add a file browser widget for loading the audio content
 class SpectrumApp(toga.App):
 
     def loadAudio(self, filename):
@@ -41,19 +39,19 @@ class SpectrumApp(toga.App):
         paud = pyaudio.PyAudio()
         format = paud.get_format_from_width(self.file.getsampwidth())
 
-        self.stream = paud.open(format = format,  
-                                channels = self.file.getnchannels(),
-                                rate = self.file.getframerate(),
-                                output = True)
+        self.stream = paud.open(format=format,
+                                channels=self.file.getnchannels(),
+                                rate=self.file.getframerate(),
+                                output=True)
 
     def playback(self, widget):
         chunk = 1024
 
         print("play")
         self.file.rewind()
-        data = self.file.readframes(chunk)  
+        data = self.file.readframes(chunk)
         while data:
-            self.stream.write(data)  
+            self.stream.write(data)
             data = self.file.readframes(chunk)
 
     def startup(self):
@@ -66,23 +64,27 @@ class SpectrumApp(toga.App):
         main_box.style.padding = 10
         main_box.style.update(alignment=CENTER)
         main_box.style.update(direction=COLUMN)
-        
-        button = toga.Button('Play', on_press=self.playback, style=Pack(width=100))
 
-        GenerateSpectrum(self.filename) 
+        button = toga.Button(
+                             'Play',
+                             on_press=self.playback,
+                             style=Pack(width=100)
+                            )
+
+        GenerateSpectrum(self.filename)
         image_from_path = toga.Image('Figure_temp.png')
         imageview_from_path = toga.ImageView(image_from_path)
         imageview_from_path.style.update(height=480)
         imageview_from_path.style.update(width=640)
         plot_box.add(imageview_from_path)
-        
+
         main_box.add(button)
         main_box.add(plot_box)
 
         self.main_window.content = main_box
 
-
         self.main_window.show()
+
 
 def main():
     return SpectrumApp('Spectrum', 'org.spectrum')
